1216bb53a6cc7428e745c9f4399945cb8e940c5a
DumbCaesar <dumbcaesar94@gmail.com>
2025-11-18 12:28:53 +0100

    passwordboxhelper to allow for password binding

---
+++ FitnessManagerWPF/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Security;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FitnessManagerWPF.Helpers
+{
+    public static class PasswordBoxHelper
+    {
+        // This attached property will hold the bound SecureString from the ViewModel
+        public static readonly DependencyProperty BoundPasswordProperty =
+            DependencyProperty.RegisterAttached(
+                "BoundPassword",                     // Name of the attached property
+                typeof(SecureString),                // Type of the property (SecureString for passwords)
+                typeof(PasswordBoxHelper),           // Owner type of the property
+                new PropertyMetadata(null, null));   // Default metadata (null, no change callback needed)
+
+        // Getter for the attached property
+        public static SecureString GetBoundPassword(DependencyObject obj)
+        {
+            return (SecureString)obj.GetValue(BoundPasswordProperty);
+        }
+
+        // Setter for the attached property
+        public static void SetBoundPassword(DependencyObject obj, SecureString value)
+        {
+            obj.SetValue(BoundPasswordProperty, value);
+        }
+
+        // This attached property determines whether the PasswordBox should "attach" the helper
+        public static readonly DependencyProperty AttachProperty =
+            DependencyProperty.RegisterAttached(
+                "Attach",                            // Name of the attached property
+                typeof(bool),                        // Boolean: true = attach, false = detach
+                typeof(PasswordBoxHelper),           // Owner type
+                new PropertyMetadata(false, OnAttachChanged)); // Callback when value changes
+
+        // Getter for Attach property
+        public static bool GetAttach(DependencyObject obj) => (bool)obj.GetValue(AttachProperty);
+
+        // Setter for Attach property
+        public static void SetAttach(DependencyObject obj, bool value) => obj.SetValue(AttachProperty, value);
+
+        // This method is called whenever the Attach property changes
+        private static void OnAttachChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // extra validation, make sure we are dealing with a PasswordBox
+            if (d is PasswordBox passwordBox)
+            {
+                // If Attach is true, subscribe to PasswordChanged event
+                if ((bool)e.NewValue)
+                {
+                    passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+                }
+                // If Attach is false, unsubscribe to prevent memory leaks
+                else
+                {
+                    passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+                }
+            }
+        }
+
+        // Event handler for PasswordBox.PasswordChanged
+        private static void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            // Cast the sender to PasswordBox
+            var passwordBox = sender as PasswordBox;
+
+            // Update the BoundPassword attached property with the current SecurePassword
+            // This triggers the binding to the LoginViewModel SecureString property
+            SetBoundPassword(passwordBox, passwordBox.SecurePassword);
+        }
+    }
+}
--- FitnessManagerWPF/ViewModel/ViewModelBase.cs
+++ FitnessManagerWPF/ViewModel/ViewModelBase.cs
@@ -3,23 +3,35 @@ using System.Runtime.CompilerServices;
 
 namespace FitnessManagerWPF.ViewModel
 {
-    public abstract class ViewModelBase : INotifyPropertyChanged
+    // Abstract class, as we don't want object creation to be possible.
+    public abstract class ViewModelBase : INotifyPropertyChanged // Implements INotifyPropertyChanged by default
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged; // PropertyChangedEventHandler
+                                                                  // is the event responsible for updating the UI
 
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) // get's the object/sender
+                                                                                        // that called the OnPropertyChanged
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); // if PropertyChanged invoke event
         }
 
         protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
         {
+            // Check if the current value (field) is equal to the new value.
+            // If they are the same, no property needs to be set
             if (Equals(field, value))
-                return false;
+                return false; // Nothing changed, so return false.
 
+            // Update the backing field with the new value
             field = value;
+
+            // Notify any data bindings that this property has changed.
+            // This triggers INotifyPropertyChanged, so the UI updates automatically.
             OnPropertyChanged(propertyName);
+
+            // Return true to indicate the value was changed
             return true;
         }
+
     }
 }
