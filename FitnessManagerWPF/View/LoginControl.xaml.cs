a043da72a0772acc7ecbf6e821f1461d83ed5cde
DumbCaesar <dumbcaesar94@gmail.com>
2025-11-18 10:15:42 +0100

    basic login layout and navigation MVVM

--- FitnessManagerWPF/View/LoginControl.xaml.cs
+++ FitnessManagerWPF/View/LoginControl.xaml.cs
@@ -1,5 +1,8 @@
-﻿using FitnessManagerWPF.ViewModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,19 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FitnessManagerWPF
+namespace FitnessManagerWPF.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginControl.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginControl : UserControl
     {
-        private MainViewModel viewModel;
-        public MainWindow()
+        public LoginControl()
         {
             InitializeComponent();
-            viewModel = new MainViewModel();
-            DataContext = viewModel;
         }
     }
 }
