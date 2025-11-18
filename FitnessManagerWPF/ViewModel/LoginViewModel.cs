8304505756f1d78c97a5d6aea23f6addb5047733
DumbCaesar <dumbcaesar94@gmail.com>
2025-11-18 12:49:56 +0100

    better ViewModelBase comments for clarity

--- FitnessManagerWPF/ViewModel/LoginViewModel.cs
+++ FitnessManagerWPF/ViewModel/LoginViewModel.cs
@@ -6,6 +6,7 @@ using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Markup;
 
 namespace FitnessManagerWPF.ViewModel
 {
--- FitnessManagerWPF/ViewModel/ViewModelBase.cs
+++ FitnessManagerWPF/ViewModel/ViewModelBase.cs
@@ -4,29 +4,32 @@ using System.Runtime.CompilerServices;
 namespace FitnessManagerWPF.ViewModel
 {
     // Abstract class, as we don't want object creation to be possible.
-    public abstract class ViewModelBase : INotifyPropertyChanged // Implements INotifyPropertyChanged by default
+    // Implements INotifyPropertyChanged, allowing property change notifications to update the UI.
+    public abstract class ViewModelBase : INotifyPropertyChanged 
     {
         public event PropertyChangedEventHandler PropertyChanged; // PropertyChangedEventHandler
-                                                                  // is the event responsible for updating the UI
+                                                                  // is the event responsible for
+                                                                  // updating the properties and UI
 
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null) // get's the object/sender
                                                                                         // that called the OnPropertyChanged
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); // if PropertyChanged invoke event
+            // If there are any subscribers, raise the PropertyChanged event, passing the property name.
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); 
         }
 
         protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
         {
             // Check if the current value (field) is equal to the new value.
-            // If they are the same, no property needs to be set
+            // If they are the same, no change is needed, and the event is not raised.
             if (Equals(field, value))
-                return false; // Nothing changed, so return false.
+                return false; // Nothing unchanged, so return false.
 
             // Update the backing field with the new value
             field = value;
 
-            // Notify any data bindings that this property has changed.
-            // This triggers INotifyPropertyChanged, so the UI updates automatically.
+            // Notify any data bindings that this property has changed via OnPropertyChanged
+            // This triggers UI updates automatically.
             OnPropertyChanged(propertyName);
 
             // Return true to indicate the value was changed
