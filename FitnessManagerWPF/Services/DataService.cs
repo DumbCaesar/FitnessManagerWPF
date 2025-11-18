9e5280be41c3ae4708203e6a21ab150a1e501432
Nicolaj Hansen <nicolaj_95@outlook.com>
2025-11-18 13:27:29 +0100

    some test data

---
+++ FitnessManagerWPF/Services/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessManagerWPF.Model;
+
+namespace FitnessManagerWPF.Services
+{
+    public class DataService
+    {
+        public List<User> LoadUsers() { }
+        public void SaveMembers() { }
+    }
+}
