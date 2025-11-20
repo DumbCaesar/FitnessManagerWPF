c83d60f44151230da89f1c6cdf8f6c6e7c5df4f8
Nicolaj Hansen <nicolaj_95@outlook.com>
2025-11-20 09:16:34 +0100

    add membership and membershipsubscription classes

---
+++ FitnessManagerWPF/Model/Membership.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessManagerWPF.Model
+{
+    /// <summary>
+    /// Membership plans offered by the gym
+    /// </summary>
+    public class Membership
+    {
+        private int _id;
+        private string _name;
+        private float _price;
+        private int _durationMonths;
+    }
+}
---
+++ FitnessManagerWPF/Model/MembershipSubscription.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessManagerWPF.Model
+{
+    /// <summary>
+    /// Memberships belonging to individual members
+    /// </summary>
+    public class MembershipSubscription
+    {
+        private int _id;
+        private Membership _membership;
+        private int _userId;
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private bool isActive;
+    }
+}
