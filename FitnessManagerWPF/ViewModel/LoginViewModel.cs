1796f2912dc11cf94c5cb077105a4102953b3d03
Nicolaj Hansen <nicolaj_95@outlook.com>
2025-11-26 12:31:07 +0100

    Add class registration and cancellation for users

--- FitnessManagerWPF/ViewModel/LoginViewModel.cs
+++ FitnessManagerWPF/ViewModel/LoginViewModel.cs
@@ -47,7 +47,7 @@ namespace FitnessManagerWPF.ViewModel
             _registerViewModel = new RegisterViewModel(this, _dataService);
 
             ShowLogin();
-            _username = "jjbigman214";
+            _username = "bo";
             _password = "password123";
         }
 
