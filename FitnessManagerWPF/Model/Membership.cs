42053b65fbd2253bda3e825d39fd6aae4d3f6d2a
DumbCaesar <dumbcaesar94@gmail.com>
2025-12-07 11:01:26 +0100

    fix wrong comment

--- FitnessManagerWPF/Model/Membership.cs
+++ FitnessManagerWPF/Model/Membership.cs
@@ -14,7 +14,7 @@ namespace FitnessManagerWPF.Model
         public decimal Price { get; set; } // The price of said membership
         public int DurationInMonths { get; set; } // number of months the subscription lasts
 
-        public int Id { get; set; } // unique id, also foreign key for a user.
+        public int Id { get; set; } // unique id, for each membership package 1-6 currently.
         public string Name { get; set; } // The membership package (basic, VIP..)
 
         public Membership() { }
