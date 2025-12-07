618251baacbeb917173a1a138f7b846c9aa80d75
DumbCaesar <dumbcaesar94@gmail.com>
2025-12-07 11:06:22 +0100

    Purchase model comments

--- FitnessManagerWPF/Model/Purchase.cs
+++ FitnessManagerWPF/Model/Purchase.cs
@@ -12,12 +12,16 @@ namespace FitnessManagerWPF.Model
     /// </summary>
     public class Purchase
     {
-        public int Id { get; set; }
-        public int MembershipId { get; set; }
-        public decimal AmountPaid { get; set; }
-        public DateTime PurchasedAt { get; set; } = DateTime.Now;
+        public int Id { get; set; } // an auto incrementing id for every purchase.
+        public int MembershipId { get; set; } // The id of the selected membership plan
+        public decimal AmountPaid { get; set; } // The price paid.
+        public DateTime PurchasedAt { get; set; } = DateTime.Now; // Get's the exact time a purchase was made.
+
+        // Reference to actual membership (not serialized)
         [JsonIgnore] public Membership Membership { get; set; }
+        // Formatted date for UI display ("12 Jan 2025 14:20")
         [JsonIgnore] public string DateDisplay => PurchasedAt.ToString("dd MMM yyyy HH:mm");
+        // Formatted price with currency symbol
         [JsonIgnore] public string AmountDisplay => AmountPaid.ToString("C2");
 
         public Purchase() { }
