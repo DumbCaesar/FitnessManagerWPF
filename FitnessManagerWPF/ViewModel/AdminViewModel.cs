2fe7e14be9122e57e1cd1c9eca0b3abf383ab361
DumbCaesar <dumbcaesar94@gmail.com>
2025-12-07 11:35:28 +0100

    AdminViewModel comments

--- FitnessManagerWPF/ViewModel/AdminViewModel.cs
+++ FitnessManagerWPF/ViewModel/AdminViewModel.cs
@@ -11,26 +11,35 @@ using FitnessManagerWPF.ViewModel.Admin;
 
 namespace FitnessManagerWPF.ViewModel
 {
+    /// <summary>
+    /// Handles navigation and state for the Admin section of the application.
+    /// </summary>
     public class AdminViewModel : ObservableObject
     {
-        private readonly DataService _dataService;
-        private User _currentUser;
-        private object _currentView;
+        private readonly DataService _dataService; // Shared data acess service
+        private User _currentUser; // the logged in user (admin)
+        private object _currentView; // used for changing the userControl to switch views.
+        // Sub-viewmodels for each admin page
         private AdminDashboardViewModel _adminDashboardViewModel;
         private AdminClassesViewModel _adminClassesViewModel;
         private AdminMemberViewModel _adminMemberViewModel;
         private AdminTrainerViewModel _adminTrainerViewModel;
-        public event Action DataChanged;
+        public event Action DataChanged; // Event for updating UI when underlying data updates.
+
+        // Commands for switching between admin pages
         public ICommand DashboardCommand { get; set; }
         public ICommand MemberCommand { get; set; }
-        public ICommand TrainerCommand { get; set; }
+        public ICommand TrainerCommand { get; set; } 
         public ICommand ClassesCommand { get; set; }
+
+        // The active view shown in the admin panel
         public object CurrentView
         {
             get => _currentView;
             set => SetProperty(ref _currentView, value);
         }
 
+        // The admin user currently logged in
         public User CurrentUser
         {
             get => _currentUser;
@@ -40,15 +49,17 @@ namespace FitnessManagerWPF.ViewModel
         {
             _currentUser = user;
             _dataService = dataService;
+            // Initialize viewmodels
             _adminClassesViewModel = new AdminClassesViewModel(this, _dataService);
             _adminDashboardViewModel = new AdminDashboardViewModel(this, _dataService);
             _adminMemberViewModel = new AdminMemberViewModel(this, _dataService);
             _adminTrainerViewModel = new AdminTrainerViewModel(this, _dataService);
+            // Setup navigation commands
             DashboardCommand = new RelayCommand(_ => ShowDashboard());
             MemberCommand = new RelayCommand(_ => ShowMembers());
             TrainerCommand = new RelayCommand(_ => ShowTrainers());
             ClassesCommand = new RelayCommand(_ => ShowClasses());
-            
+            // Default page (start page)
             ShowDashboard();
         }
 
@@ -57,6 +68,7 @@ namespace FitnessManagerWPF.ViewModel
             
         }
 
+        // The command methods for switching to a different view.
         public void ShowDashboard()
         {
             CurrentView = _adminDashboardViewModel;
@@ -77,7 +89,7 @@ namespace FitnessManagerWPF.ViewModel
             CurrentView = _adminClassesViewModel;
         }
 
-        public void NotifyDataChanged()
+        public void NotifyDataChanged() // The event that get's invoked
         {
             DataChanged?.Invoke();
         }
